6fac2a2ae29238e9e1bbc5152ef9c6834eacfef1
Tom-Ole Korbach <Tomolekorbach@gmail.com>
2025-08-25 22:30:40 +0200

    add pnm

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,32 @@
 const std = @import("std");
 const bmp = @import("formats/bmp.zig");
+const pnm = @import("formats/pnm.zig");
 
 pub fn main() !void {
+    const alloc = std.heap.page_allocator;
+
+    const width = 300;
+    const height = 300;
+    var test_data: []u8 = try alloc.alloc(u8, width * height * 3);
+    defer alloc.free(test_data);
+
+    for (0..height) |y| {
+        for (0..width) |x| {
+            const r: u8 = @intCast((y * 255) / height);
+            const b: u8 = @intCast((x * 255) / width);
+
+            const idx = (y * width + x) * 3;
+            test_data[idx] = r;
+            test_data[idx + 1] = 0;
+            test_data[idx + 2] = b;
+        }
+    }
+
+    const image = pnm.create(width, height, test_data, pnm.FILE_TYPES.PIXMAP_ASCII);
+    try image.create_image("Test.ppm");
+}
+
+test "bpm" {
     const alloc = std.heap.page_allocator;
 
     const width = 300;
